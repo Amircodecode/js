9892ff8f5d989da39d0e260d17a70d961f567ff4
amir <a3465172@gmail.com>
2024-11-10 12:42:56 +0500

    s

--- index.js
+++ index.js
@@ -385,3 +385,13 @@
 
 // setTimeout(sumPositiveNumbers(2,4), 1000)
 
+const month = 2
+
+switch (month) {
+    case 1:
+        console.log('January')
+        break
+    case 2:
+        console.log('February')
+        break
+}
